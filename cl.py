3d586d2aef263d5e1c0114945f9ed13ba88502f7
tepo18 <ahsan.tepo1380@gmail.com>
2026-08-28 21:59:02 +0330

    Update cl.py
    fvbbn

--- cl.py
+++ cl.py
@@ -15,24 +15,17 @@ from typing import List
 TEXT_PATH = "normal.txt"
 FIN_PATH = "final.txt"
 LINK_PATH = [
-    "https://raw.githubusercontent.com/hamedp-71/v2go_NEW/main/Splitted-By-Protocol/ss.txt",
-    "https://raw.githubusercontent.com/tepo18/tepo90/main/ss.txt",
-    "https://raw.githubusercontent.com/tepo18/reza-shah1320/main/ss.txt",
-    "https://raw.githubusercontent.com/tepo18/online-sshmax98/main/ss.txt",
-    "https://raw.githubusercontent.com/MatinGhanbari/v2ray-configs/main/subscriptions/filtered/subs/ss.txt",
-    "https://raw.githubusercontent.com/youfoundamin/V2rayCollector/main/ss_iran.txt",
-    "https://raw.githubusercontent.com/dream4network/telegram-configs-collector/main/protocols/shadowsocks",
-    "https://github.com/barry-far/V2ray-Configs/raw/main/Splitted-By-Protocol/ss.txt",
-    "https://github.com/Kwinshadow/TelegramV2rayCollector/raw/main/sublinks/b64ss.txt",
-    "https://raw.githubusercontent.com/iboxz/free-v2ray-collector/main/main/shadowsocks",
-    "https://raw.githubusercontent.com/mermeroo/free-v2ray-collector/main/main/shadowsocks",
-    "https://raw.githubusercontent.com/yebekhe/TelegramV2rayCollector/main/sub/normal/shadowsocks",
-    "https://raw.githubusercontent.com/Surfboardv2ray/TGParse/main/splitted/ss",
-    "https://raw.githubusercontent.com/mheidari98/.proxy/refs/heads/main/ss",
-    "https://raw.githubusercontent.com/Huibq/TrojanLinks/master/links/ss",
-    "https://raw.githubusercontent.com/Huibq/TrojanLinks/master/links/ss_with_plugin",
-    "https://raw.githubusercontent.com/MhdiTaheri/V2rayCollector/main/sub/ssbase64",
-    "https://raw.githubusercontent.com/vxiaov/free_proxy_ss/main/ss/sssub"
+    "https://raw.githubusercontent.com/tepo18/sab-vip10/main/shah.txt",
+    "https://billowing-king-badd.shah98-tepo98.workers.dev/sub?token=ad2787e72ea01b52a90cc686afe1896f",
+    "https://almasi.ahsan-tepo1383online.workers.dev/sub/sub",
+    "https://xnzvhfevu8ms.shah98-tepo98.workers.dev/feed/ZEUS-77IELBGK",
+    "https://small-pond-676-e-d-g-e.batool-sogeli.workers.dev/sub?token=54e533fc74493379c95453c419066252",
+    "https://wlzmgdefumms.ahsan-tepo1390.workers.dev/feed/jjjjjjjjjjjjj",
+    "https://avopt-efoxs7qnam.pages.dev/Gp5phgTfSXuWNe/sub/raw?app=xray#%F0%9F%92%A6%20BPB%20Raw",
+    "https://n-h-a-n-98.ahsan-tepo1383online.workers.dev/sync?sub=d5d4cc2f47b3c71bf88e5a2b9cc9de4b&flag=raw",
+    "https://raw.githubusercontent.com/patterniha/Free-Configs/main/configs.txt",
+    "https://frosty-robin-2c97.tepo3360.workers.dev/sub?token=69c2fbc1aeb0204a096e60dc14f09370",
+    "https://xnzvhfevu8ms.shah98-tepo98.workers.dev/feed/ZEUS-77IELBGK"
 ]
 
 FILE_HEADER_TEXT = "//profile-title: base64:2YfZhduM2LTZhyDZgdi52KfZhCDwn5iO8J+YjvCfmI4gaGFtZWRwNzE="
